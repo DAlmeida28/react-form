4ba303f5cdca92582c0b1a52d41c7440bfe25d48
Daniel Almeida <almeida.danny@gmail.com>
2025-02-06 17:04:50 -0600

    Add Authentication

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,5 +1,37 @@
-const Authentication = () => {
-  return <h2>Authenticate</h2>
+import { useState } from "react"
+
+const Authentication = ({ token }) => {
+const [successMessage, setSuccessmessage] = useState('');
+const [error, setError] = useState('');
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate',{
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        }
+      })
+      const result = await response.json();
+      setSuccessmessage(result.message);
+    } catch (err) {
+      setError(err.message);
+    }
+
+    
+  }
+
+  return ( 
+    <>
+      <h2>Authenticate</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <button
+        onClick={handleClick}
+      >Authenticate Token</button>
+    </>
+  )
 }
 
 export default Authentication
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-const SignUp = () => {
+const SignUp = ({setToken}) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
@@ -21,7 +21,7 @@ const SignUp = () => {
       });
 
       const result = await response.json();
-      console.log(result);
+      setToken(result.token);
     } catch (err) {
       setError(err.message);
     }
